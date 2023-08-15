631d7a74b9e03cc0af963f8856f1a8cdb4f70189
Shaaf, Syed <sshaaf@redhat.com>
2023-08-15 16:07:40 +0200

    updates for devspaces

--- docs/runfile.sh
+++ docs/runfile.sh
@@ -2,10 +2,10 @@
 
 podman run -it --rm -p 4000:8080 -v $(pwd):/app-data  \
   -e MASTER_URL=http://your.master.com:1111 \
-  -e CONSOLE_URL=http://your.console.url.com:2222 \
-  -e CHE_URL=http://codeready.url.com:3333 -e RHAMT_URL=http://foo -e CHE_USER_PASSWORD='r3dh4t1!' \
+  -e CONSOLE_URL=https://console-openshift-console.apps.cluster-c6rpn.c6rpn.sandbox1073.opentlc.com \
+  -e CHE_URL=https://devspaces.apps.cluster-c6rpn.c6rpn.sandbox1073.opentlc.com/ -e RHAMT_URL=http://foo -e CHE_USER_PASSWORD='r3dh4t1!' \
   -e KEYCLOAK_URL=http://keycloak.url.com:4444 \
-  -e ROUTE_SUBDOMAIN=apps.cluster-m8dqv.m8dqv.sandbox2261.opentlc.com \
+  -e ROUTE_SUBDOMAIN=apps.cluster-c6rpn.c6rpn.sandbox1073.opentlc.com \
   -e CONTENT_URL_PREFIX="file:///app-data/" \
   -e WORKSHOPS_URLS="file:///app-data/_sso-workshop-guides.yml" \
   -e USER_NAME=userNN \
